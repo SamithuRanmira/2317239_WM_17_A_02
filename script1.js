ca0f082e2309c3c8e726df1d9fc717f15931483d
JPSRJ <144013182+JPSRJ@users.noreply.github.com>
2023-11-11 02:15:28 +0530

    update

---
+++ script1.js
@@ -0,0 +1,11 @@
+function getUsersName(){
+
+    const userName1 = document.getElementById("input1").value;
+    const userName2 = document.getElementById("input2").value;
+
+    localStorage.setItem("Player_01", userName1);
+    localStorage.setItem("Player_02", userName2);
+    
+    return;
+
+}
